230f3997ccfdc53623bc1cb01d8ec95737912229
Till Hoffmann <tillahoffmann@gmail.com>
2022-10-07 09:17:41 -0400

    Add Stockholm model implementation with tests.

---
+++ tests/test_stockholm.py
@@ -0,0 +1,54 @@
+import logging
+import networkx as nx
+import numpy as np
+import pytest
+import stockholm
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+@pytest.fixture(params=[
+    {"n": 50, "w0": 0.2, "w1": 0.1, "mu": 0.3, "rho": 0.4, "sigma": 0.5},
+])
+def params(request: pytest.FixtureRequest) -> dict[str, float]:
+    return request.param
+
+
+def test_add_nodes_from_update() -> None:
+    graph = nx.Graph()
+    graph.add_nodes_from(range(5), a="a")
+    updated = {2, 3, 7}
+    graph.add_nodes_from(updated, a="b")
+    for node in updated:
+        assert graph.nodes[node]["a"] == "b"
+    for node in set(graph) - updated:
+        assert graph.nodes[node]["a"] == "a"
+
+    assert graph.number_of_nodes() == 6
+
+
+def test_multi_step(params: dict[str, float]) -> None:
+    seed = 0
+    num_steps = 2
+    np.random.seed(seed)
+    LOGGER.info("starting multi-step simulation...")
+    graph1, statistics1 = stockholm.simulate(**params, num_steps=num_steps)
+    np.random.seed(seed)
+    LOGGER.info("starting sequential simulation...")
+    graph2 = nx.Graph()
+    for step in range(num_steps):
+        _, statistics2 = stockholm.simulate(**params, num_steps=1, step=step, graph=graph2)
+
+    # Verify we have the same graph and statistics.
+    stockholm.assert_graphs_equal(graph1, graph2)
+    assert set(statistics1) == set(statistics2)
+    for key in statistics1:
+        np.testing.assert_allclose(statistics1[key][-1], statistics2[key][-1])
+
+    # Verify expected values.
+    for key, graph, statistics in [("multi", graph1, statistics1), ("seq", graph2, statistics2)]:
+        assert stockholm.evaluate_num_edges(graph, True) == statistics["num_casual_edges"][-1], \
+            f"number of casual relationships is wrong for {key}"
+        assert stockholm.evaluate_num_edges(graph, False) == statistics["num_steady_edges"][-1], \
+            f"number of steady relationships is wrong for {key}"
