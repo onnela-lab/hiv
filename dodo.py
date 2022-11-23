76d6534fca6d050c03eebf8d569103fa5c6bc85b
Till Hoffmann <tillahoffmann@gmail.com>
2022-11-23 15:37:10 -0500

    Add targets for different prior parameterizations.

--- dodo.py
+++ dodo.py
@@ -17,8 +17,40 @@ split_sizes = {
 }
 n = 200
 
-for split, (size, kwargs) in split_sizes.items():
-    target = f"workspace/{split}.pkl"
-    action = ["python", "-m" "hiv.scripts.generate_data", size, n, 50, target]
-    action.extend(di.dict2args(kwargs or {}))
-    manager(basename="simulations", name=split, targets=[target], actions=[action])
+# Different prior parameterizations. The smaller the parameters, the longer the time scale over
+# which the graphs evolve.
+priors = {
+    "medium": {
+        "mu": (2, 2),
+        "rho": (2, 2),
+        "sigma": (2, 2),
+        "w0": (2, 2),
+        "w1": (2, 2),
+    },
+    "small": {
+        "mu": (2, 18),
+        "rho": (2, 18),
+        "sigma": (2, 18),
+        "w0": (2, 18),
+        "w1": (2, 18),
+    },
+    "x-small": {
+        "mu": (2, 48),
+        "rho": (2, 48),
+        "sigma": (2, 48),
+        "w0": (2, 48),
+        "w1": (2, 48),
+    },
+}
+
+for name, prior in priors.items():
+    for split, (size, kwargs) in split_sizes.items():
+        target = f"workspace/{name}/{split}.pkl"
+        action = ["python", "-m", "hiv.scripts.generate_data", size, n, 100, target]
+        if split.startswith("debug"):
+            action.append("--save_graphs")
+        kwargs = kwargs or {}
+        kwargs.update({f"{key}_prior": ",".join(map(str, params)) for key, params in prior.items()})
+        action.extend(di.dict2args(kwargs))
+        manager(basename=f"simulations-{name}", name=split, targets=[target], actions=[action],
+                uptodate=[True])
