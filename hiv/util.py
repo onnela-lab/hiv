5f685f0fe7644e390d30e37c2ff4da7fddfea443
Till Hoffmann <tillahoffmann@gmail.com>
2024-07-18 16:45:24 -0400

    Apply `black` formatting.

--- hiv/util.py
+++ hiv/util.py
@@ -8,9 +8,12 @@ def assert_graphs_equal(actual: nx.Graph, expected: nx.Graph) -> None:
     Assert that two graphs have the same nodes, edges, and attributes.
     """
     assert dict(actual.nodes(data=True)) == dict(expected.nodes(data=True))
-    assert {tuple(edge): data for *edge, data in actual.edges(data=True)} \
-        == {tuple(edge): data for *edge, data in expected.edges(data=True)}
+    assert {tuple(edge): data for *edge, data in actual.edges(data=True)} == {
+        tuple(edge): data for *edge, data in expected.edges(data=True)
+    }
 
 
-def to_np_dict(x: dict[typing.Hashable, typing.Iterable]) -> dict[typing.Hashable, np.ndarray]:
+def to_np_dict(
+    x: dict[typing.Hashable, typing.Iterable]
+) -> dict[typing.Hashable, np.ndarray]:
     return {key: np.asarray(value) for key, value in x.items()}
