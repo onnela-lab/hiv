1f40a68e2518cd92346c07d05d1dcd4c401ff2fe
Till Hoffmann <tillahoffmann@gmail.com>
2025-06-24 14:55:05 -0400

    Use fixed seed for random tests.

--- conftest.py
+++ conftest.py
@@ -1,4 +1,11 @@
 import os
-
+import numpy as np
+import pytest
 
 os.environ["CI"] = "True"
+
+
+@pytest.fixture(autouse=True)
+def set_random_seed():
+    """Set a fixed random seed for reproducible tests."""
+    np.random.seed(42)
